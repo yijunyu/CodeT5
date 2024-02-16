84fb635b089f5603ce42de9bcabdc1effd3ecfe4
Yijun Yu <y.yu@open.ac.uk>
2024-02-17 02:19:08 +0800

    fine-tune the CodeT5+ model with the C-to-Rust datasets yijunyu/c-to-rust

--- CodeT5+/tune_codet5p_seq2seq.py
+++ CodeT5+/tune_codet5p_seq2seq.py
@@ -5,12 +5,14 @@ Author: Yue Wang
 Date: June 2023
 """
 
+import urllib3
+urllib3.disable_warnings()
 import os
 import pprint
 import argparse
 from datasets import load_dataset, load_from_disk
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, TrainingArguments, Trainer
-
+import sys
 
 def run_training(args, model, train_data):
     print(f"Starting main loop")
@@ -42,6 +44,7 @@ def run_training(args, model, train_data):
         local_rank=args.local_rank,
         deepspeed=args.deepspeed,
         fp16=args.fp16,
+        optim='adamw_torch',
     )
 
     trainer = Trainer(
@@ -66,12 +69,15 @@ def load_tokenize_data(args):
         return train_data
     else:
         # Example code to load and process code_x_glue_ct_code_to_text python dataset for code summarization task
-        datasets = load_dataset("code_x_glue_ct_code_to_text", 'python', split="train")
+        # datasets = load_dataset("code_x_glue_ct_code_to_text", 'python', split="train")
+        datasets = load_dataset("yijunyu/c-to-rust", 'c-to-rust')
         tokenizer = AutoTokenizer.from_pretrained(args.load)
 
         def preprocess_function(examples):
-            source = [' '.join(ex) for ex in examples["code_tokens"]]
-            target = [' '.join(ex) for ex in examples["docstring_tokens"]]
+            # source = [' '.join(ex) for ex in examples["code_tokens"]]
+            # target = [' '.join(ex) for ex in examples["docstring_tokens"]]
+            source = [' '.join(ex) for ex in examples["before"]]
+            target = [' '.join(ex) for ex in examples["after"]]
 
             model_inputs = tokenizer(source, max_length=args.max_source_len, padding="max_length", truncation=True)
             labels = tokenizer(target, max_length=args.max_target_len, padding="max_length", truncation=True)
@@ -81,11 +87,11 @@ def load_tokenize_data(args):
                 [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in model_inputs["labels"]
             ]
             return model_inputs
-
-        train_data = datasets.map(
+        print(datasets.column_names["train"])
+        train_data = datasets["train"].map(
             preprocess_function,
             batched=True,
-            remove_columns=datasets.column_names,
+            remove_columns=datasets["train"].column_names,
             num_proc=64,
             load_from_cache_file=False,
         )
@@ -111,8 +117,13 @@ def main(args):
         train_data = train_data.select([i for i in range(args.data_num)])
 
     # Load model from `args.load`
-    model = AutoModelForSeq2SeqLM.from_pretrained(args.load)
-    print(f"  ==> Loaded model from {args.load}, model size {model.num_parameters()}")
+    model_file = os.path.join(args.save_dir, "final_checkpoint")
+    if os.path.exists(model_file):
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_file)
+        print(f"  ==> Loaded model from {args.save_dir}, model size {model.num_parameters()}")
+    else:
+        model = AutoModelForSeq2SeqLM.from_pretrained(args.load)
+        print(f"  ==> Loaded model from {args.load}, model size {model.num_parameters()}")
 
     run_training(args, model, train_data)
 
@@ -121,22 +132,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="CodeT5+ finetuning on Seq2Seq LM task")
     parser.add_argument('--data-num', default=-1, type=int)
     parser.add_argument('--max-source-len', default=320, type=int)
-    parser.add_argument('--max-target-len', default=128, type=int)
-    parser.add_argument('--cache-data', default='cache_data/summarize_python', type=str)
+    parser.add_argument('--max-target-len', default=320, type=int)
+    parser.add_argument('--cache-data', default='cache_data/c_to_rust', type=str)
     parser.add_argument('--load', default='Salesforce/codet5p-220m', type=str)
 
     # Training
-    parser.add_argument('--epochs', default=10, type=int)
+    parser.add_argument('--epochs', default=60, type=int)
     parser.add_argument('--lr', default=5e-5, type=float)
     parser.add_argument('--lr-warmup-steps', default=200, type=int)
-    parser.add_argument('--batch-size-per-replica', default=8, type=int)
+    parser.add_argument('--batch-size-per-replica', default=16, type=int)
     parser.add_argument('--grad-acc-steps', default=4, type=int)
     parser.add_argument('--local_rank', default=-1, type=int)
     parser.add_argument('--deepspeed', default=None, type=str)
     parser.add_argument('--fp16', default=False, action='store_true')
 
     # Logging and stuff
-    parser.add_argument('--save-dir', default="saved_models/summarize_python", type=str)
+    parser.add_argument('--save-dir', default="saved_models/c_to_rust", type=str)
     parser.add_argument('--log-freq', default=10, type=int)
     parser.add_argument('--save-freq', default=500, type=int)
 
